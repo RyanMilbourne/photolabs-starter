8c6cba983497a122508eb9ed10e0d608a0a2563d
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-01-29 14:09:06 -0800

    add clean-up

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,25 +7,16 @@ const PhotoListItem = (props) => {
   const { imageSource, profile, username, location } = props.data;
   return (
     <div className="photo-list__item">
-
       <img className="photo-list__image" src={imageSource}></img>
-
       <div className="photo-list__user-details">
-
         <img className="photo-list__user-profile" src={profile}></img>
-
         <div className="photo-list__user-info">
-
           <span>{username}</span>
-
           <div className="photo-list__user-location">
             <span>{location.city}, {location.country}</span>
           </div>
-
         </div>
-
       </div>
-
     </div>
   )
 };
