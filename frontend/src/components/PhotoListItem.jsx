cfb4cbeae6cacac90a8d20bb6e2026ffd895413d
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-01-29 12:55:47 -0800

    add destructuring of props data}

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,21 +3,23 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
+
+  const { imageSource, profile, username, location } = props.data;
   return (
     <div className="photo-list__item">
 
-      <img className="photo-list__image" src={props.data.imageSource}></img>
+      <img className="photo-list__image" src={imageSource}></img>
 
       <div className="photo-list__user-details">
 
-        <img className="photo-list__user-profile" src={props.data.profile}></img>
+        <img className="photo-list__user-profile" src={profile}></img>
 
         <div className="photo-list__user-info">
 
-          <span>{props.data.username}</span>
+          <span>{username}</span>
 
           <div>
-            <span className="photo-list__user-location">{props.data.location.city}, {props.data.location.country}</span>
+            <span className="photo-list__user-location">{location.city}, {location.country}</span>
           </div>
 
         </div>
