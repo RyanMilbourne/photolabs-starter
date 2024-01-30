bb08dde514697f83ff60043c5e11ddd9dbe31619
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-01-29 16:11:41 -0800

    add single component for developing and comment out other components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,26 +3,16 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoFavButton from 'components/PhotoFavButton';
+import PhotoList from 'components/PhotoList';
 
 const App = () => {
 
-  const sampleDataForPhotoListItem = {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  };
-
-  const photoArray = [1, 2, 3];
+  // const photoArray = [1, 2, 3];
 
   return (
     <div className="App">
-
-      {photoArray.map((index) => (<PhotoListItem key={index} data={sampleDataForPhotoListItem} />))}
+      {/* {photoArray.map((index) => (<PhotoListItem key={index} data={sampleDataForPhotoListItem} />))} */}
+      <PhotoList />
     </div>
   );
 };
