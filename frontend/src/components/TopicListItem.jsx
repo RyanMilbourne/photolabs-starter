ade854f2b09d5a45a23b7f19199bf03ac4847265
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-01-29 20:05:53 -0800

    display topics on navbar

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/TopicListItem.scss";
+import TopicList from "./TopicList";
 
 const sampleDataForTopicListItem = {
   id: "1",
@@ -9,9 +10,10 @@ const sampleDataForTopicListItem = {
 };
 
 const TopicListItem = () => {
+
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
+      <TopicList />
     </div>
   );
 };
