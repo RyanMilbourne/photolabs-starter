423340866245ed16d3333e51fb5fb69ff6fae171
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-01-29 20:25:05 -0800

    display TopicList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-import PhotoFavButton from 'components/PhotoFavButton';
 import PhotoList from 'components/PhotoList';
+import TopicList from 'components/TopicList';
 
 const App = () => {
 
   return (
     <div className="App">
+      <TopicList />
       <PhotoList />
     </div>
   );
