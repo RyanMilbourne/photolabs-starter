4874db0374c3ac09e4ec80a7f93ab52afe7f5fc5
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-01-29 20:44:33 -0800

    add mock data and cleanup

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,28 +2,11 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "mocks/topics";
 
 const TopicList = () => {
 
-  const data = sampleDataForTopicList.map((item) => (
+  const data = topics.map((item) => (
     <TopicListItem key={item.id} data={item} />
   ));
 
