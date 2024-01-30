7a4ad1981b33bf95dec91c2e612767d4620c3e19
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-01-29 20:34:58 -0800

    import TopNav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './App.scss';
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 
 const App = () => {
 
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigation />
       <PhotoList />
     </div>
   );
