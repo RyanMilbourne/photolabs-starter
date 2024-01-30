91aa22cb63346a65a4eae910a286601d00d62340
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-01-29 20:39:25 -0800

    import HomeRoute and remove solo components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
 import './App.scss';
-import PhotoList from 'components/PhotoList';
-import TopNavigation from 'components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 
 const App = () => {
-
   return (
     <div className="App">
-      <TopNavigation />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
