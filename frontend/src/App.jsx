3bda8ccc624b6e549978eacf6ffc79e3c42dfd32
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-01-29 16:13:45 -0800

    remove comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,8 @@ import PhotoList from 'components/PhotoList';
 
 const App = () => {
 
-  // const photoArray = [1, 2, 3];
-
   return (
     <div className="App">
-      {/* {photoArray.map((index) => (<PhotoListItem key={index} data={sampleDataForPhotoListItem} />))} */}
       <PhotoList />
     </div>
   );
